bfd3d37ffd7e303444ae74cdee69d4a411760f69
so hosoki <fucso.h@gmail.com>
2026-07-30 23:48:42 +0900

    chore: remove worktree tmp files from tracked changes

--- tmp/do_fix.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-cd /Users/sohosoki/dev/fucso/bake-loose/.worktree/iddue/37
-git rm -r --cached tmp >/dev/null
-git commit -m "chore: remove worktree tmp files from tracked changes"
--- tmp/run_qc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bash /Users/sohosoki/.claude/plugins/cache/iddue/iddue/d067a2bf66f4/bin/worktree-quality-check.sh "iddue/37" > /Users/sohosoki/dev/fucso/bake-loose/.worktree/iddue/37/tmp/quality-check.log 2>&1
-echo "EXIT:$?"
